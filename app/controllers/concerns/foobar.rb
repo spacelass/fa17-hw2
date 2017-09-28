b2160bfadfaab558ee1c1a7dfb0f2618553e646e
Mumu Lin <mumulin@berkeley.edu>
2017-09-27 17:02:19 -0700

    Unable to do question 4 see Piazza post

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,5 +1,11 @@
 class Foobar
-  # ENTER CODE FOR Q2 HERE
+  attr_accessor :word
   def initialize(baz)
+  	@word = baz
   end
+
+  def bar(x, y)
+  	return x.to_s + @word + y[:sat].to_s
+  end
+
 end
